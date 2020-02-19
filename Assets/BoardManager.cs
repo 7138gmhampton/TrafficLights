2d720e8c34502a037c6c7301253de8023f26a060
7138gmhampton <0407435@abertay.ac.uk>
2020-02-19 18:15:24 +0000

    Add junction lay method
    Add method that lays the junction tiles in their proper orientation

--- Assets/BoardManager.cs
+++ Assets/BoardManager.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
+    private enum Corner
+    {
+        TOP_LEFT,
+        TOP_RIGHT,
+        BOTTOM_LEFT,
+        BOTTOM_RIGHT
+    }
+
     private const int UP = 1;
     private const int RIGHT = 2;
     private const int DOWN = 3;
     private const int LEFT = 4;
+    private const int JUNCTION = 5;
 
     public GameObject terrainTile;
     public GameObject roadTile;
+    public GameObject junctionTile;
 
     private readonly int[][] roadLocations = new int[][]
         {
             new int[] { 0,0,1,3,0,0 },
             new int[] { 0,0,1,3,0,0 },
-            new int[] { 0,0,1,3,0,0 },
-            new int[] { 0,0,1,3,0,0 },
+            new int[] { 0,0,5,5,0,0 },
+            new int[] { 0,0,5,5,0,0 },
             new int[] { 0,0,1,3,0,0 },
             new int[] { 0,0,1,3,0,0 }
         };
@@ -48,9 +58,45 @@ public class BoardManager : MonoBehaviour
                     case DOWN:
                         Instantiate(roadTile, new Vector2(column, row), Quaternion.Euler(0,0,180f));
                         break;
+                    case JUNCTION:
+                        layJunctionTile(new Vector2(column, row));
+                        break;
                     default:
                         Instantiate(terrainTile, new Vector2(column, row), Quaternion.identity);
                         break;
                 }
+    }
+
+    private void layJunctionTile(Vector2 locus)
+    {
+        Corner corner = determineCorner(locus);
+
+        switch (corner) {
+            case Corner.TOP_LEFT:
+                Instantiate(junctionTile, locus, Quaternion.Euler(0, 0, 270));
+                break;
+            case Corner.TOP_RIGHT:
+                Instantiate(junctionTile, locus, Quaternion.Euler(0, 0, 180));
+                break;
+            case Corner.BOTTOM_LEFT:
+                Instantiate(junctionTile, locus, Quaternion.Euler(0, 0, 0));
+                break;
+            case Corner.BOTTOM_RIGHT:
+                Instantiate(junctionTile, locus, Quaternion.Euler(0, 0, 90));
+                break;
+        }
+    }
+
+    private Corner determineCorner(Vector2 locus)
+    {
+        int above = roadLocations[(int)locus.y + 1][(int)locus.x];
+        int after = roadLocations[(int)locus.y][(int)locus.x + 1];
+        int below = roadLocations[(int)locus.y - 1][(int)locus.x];
+        int before = roadLocations[(int)locus.y][(int)locus.x - 1];
+
+        if (after == 5 && below == 5) return Corner.TOP_LEFT;
+        else if (before == 5 && below == 5) return Corner.TOP_RIGHT;
+        else if (after == 5) return Corner.BOTTOM_LEFT;
+        else return Corner.BOTTOM_RIGHT;
     }
 }
