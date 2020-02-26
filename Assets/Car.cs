7d78ebe30d71fc0189d726fdcd4461ac985851d6
7138gmhampton <0407435@abertay.ac.uk>
2020-02-26 17:27:16 +0000

    Create car script with smooth movement
    Create script for car and add method to move a car smoothly to a new locus. Also apply this script to the prefab

---
+++ Assets/Car.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float moveTime;
+
+    private Rigidbody2D carRigidbody;
+    private float inverseMoveTime;
+
+    void Start()
+    {
+        carRigidbody = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+    }
+
+    void Update()
+    {
+        var nextTile = transform.position;
+        nextTile.y += 1.0f;
+
+        StartCoroutine(smoothMovement(nextTile));
+    }
+
+    private IEnumerator smoothMovement(Vector3 end)
+    {
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        while (sqrRemainingDistance > float.Epsilon) {
+            Vector3 nextPosition = Vector3.MoveTowards(carRigidbody.position, end, inverseMoveTime * Time.deltaTime);
+            carRigidbody.MovePosition(nextPosition);
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+            yield return null;
+        }
+    }
+}
