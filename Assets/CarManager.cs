adea179b21256274fb05857ca68a220a2bccb45e
7138gmhampton <0407435@abertay.ac.uk>
2020-02-28 14:56:18 +0000

    Add list to contain instances of the spawners

--- Assets/CarManager.cs
+++ Assets/CarManager.cs
@@ -11,6 +11,7 @@ public class CarManager : MonoBehaviour
     private int xEnd;
     private int yStart;
     private int yEnd;
+    private List<GameObject> spawners = new List<GameObject>();
 
     public int XStart { set { xStart = value; } }
     public int XEnd { set { xEnd = value; } }
@@ -22,6 +23,7 @@ public class CarManager : MonoBehaviour
         //placeSpawnersAll();
         xStart = 0;
         yStart = 0;
+        //spawners = new List<GameObject>();
     }
 
     void Update()
@@ -32,20 +34,38 @@ public class CarManager : MonoBehaviour
     public void placeSpawnersAll()
     {
         placeSpawnersHorizontal(0, 2);
-        placeSpawnersHorizontal(yEnd, 3);
-        placeSpawnersVertical(0, 3);
-        placeSpawnersVertical(xEnd, 2);
+        //placeSpawnersHorizontal(yEnd, 3);
+        //placeSpawnersVertical(0, 3);
+        //placeSpawnersVertical(xEnd, 2);
     }
 
     private void placeSpawnersHorizontal(int yAxis, int column)
     {
-        for (int x = 0; x < xEnd; ++x)
-            if (x % 6 == column) Instantiate(spawner, new Vector3(x, yAxis, 0f), Quaternion.identity);
+        for (int x = 0; x < xEnd; ++x) {
+            //var nextSpawner = spawner;
+            //spawners.Add(nextSpawner);
+            if (x % 6 == column) {
+                var nextSpawner = Instantiate(spawner, new Vector3(x, yAxis, 0f), Quaternion.identity);
+                spawners.Add(nextSpawner);
+            }
+        }
     }
 
     private void placeSpawnersVertical(int xAxis, int row)
     {
         for (int y = 0; y < yEnd; ++y)
             if (y % 6 == row) Instantiate(spawner, new Vector3(xAxis, y, 0f), Quaternion.identity);
+    }
+
+    private void spawnCar()
+    {
+        Instantiate(car, selectRandomSpawner(), Quaternion.identity);
+    }
+
+    private Vector3 selectRandomSpawner()
+    {
+        var spawnPoint = spawners[Random.Range(0, spawners.Count)];
+
+        return spawnPoint.transform.position;
     }
 }
