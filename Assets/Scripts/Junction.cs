0ec4adcdc4d76bd405d58c7dd129b59e4a12cd2b
7138gmhampton <0407435@abertay.ac.uk>
2020-02-29 21:25:30 +0000

    Add object field to junction struct

--- Assets/Scripts/Junction.cs
+++ Assets/Scripts/Junction.cs
@@ -1,13 +1,17 @@
-﻿public struct Junction
+﻿using UnityEngine;
+
+public struct Junction
 {
     public int XLocus { get; }
     public int YLocus { get; }
+    public GameObject Object { get; }
     public JunctionController Controller { get; }
 
-    public Junction(int xLocus, int yLocus, JunctionController controller)
+    public Junction(int xLocus, int yLocus, GameObject gameObject, JunctionController controller)
     {
         XLocus = xLocus;
         YLocus = yLocus;
+        Object = gameObject;
         Controller = controller;
     }
 }
