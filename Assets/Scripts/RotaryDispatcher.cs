35b347faf24c13f2377554cefe549f6f4f4fad28
7138gmhampton <0407435@abertay.ac.uk>
2020-02-29 22:43:28 +0000

    Remove dead code

--- Assets/Scripts/RotaryDispatcher.cs
+++ Assets/Scripts/RotaryDispatcher.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RotaryDispatcher : MonoBehaviour
 {
@@ -8,25 +6,15 @@ public class RotaryDispatcher : MonoBehaviour
 
     private float ticker = 0f;
     private int counter = 0;
-    private int counterMax;
-
-    void Start()
-    {
-        counterMax = lightsController.junctions.Count;
-        Debug.Log(counterMax);
-    }
 
     void Update()
     {
         ticker += Time.deltaTime;
-        //Debug.Log(lightsController.junctions.Count);
 
         if (ticker > 3f) {
             ticker = 0f;
 
             var nextJunction = lightsController.junctions[counter++];
-            //bool previousAlignment = nextJunction.Controller.northSouthAlign;
-            //nextJunction.Controller.goGreenNorthSouth(!previousAlignment);
             int xCursor = nextJunction.XLocus;
             int yCursor = nextJunction.YLocus;
 
