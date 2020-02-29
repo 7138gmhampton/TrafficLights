467e6f98afb1e8a473683ea48627d076fe4328b4
7138gmhampton <0407435@abertay.ac.uk>
2020-02-29 17:36:11 +0000

    Create controller for all lights and struct describing a junction

---
+++ Assets/Scripts/AllLightsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllLightsController : MonoBehaviour
+{
+    [HideInInspector] public AllLightsController instance = null;
+    [HideInInspector] public List<Junction> junctions = new List<Junction>();
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Junction.cs
@@ -0,0 +1,11 @@
+﻿public struct Junction
+{
+    public int XLocus { get; }
+    public int YLocus { get; }
+
+    public Junction(int xLocus, int yLocus)
+    {
+        XLocus = xLocus;
+        YLocus = yLocus;
+    }
+}
