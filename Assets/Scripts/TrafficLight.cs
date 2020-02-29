2432e8c009d5746712986c984c4c3703e44dfc9e
7138gmhampton <0407435@abertay.ac.uk>
2020-02-29 15:32:01 +0000

    Add method to change light colour

---
+++ Assets/Scripts/TrafficLight.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour
+{
+    public Sprite greenLight;
+    public Sprite redLight;
+
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void goGreen(bool green)
+    {
+        if (green) {
+            spriteRenderer.sprite = greenLight;
+            boxCollider2D.enabled = false;
+        }
+        else {
+            spriteRenderer.sprite = redLight;
+            boxCollider2D.enabled = true;
+        }
+    }
+}
