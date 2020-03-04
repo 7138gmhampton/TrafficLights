a26646e7b1823b744dadd374cea7e8cb3995b781
7138gmhampton <0407435@abertay.ac.uk>
2020-03-04 17:29:30 +0000

    Move rule struct into its own file

---
+++ Assets/Scripts/ZoneCreator.Rule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public partial class ZoneCreator
+{
+    private struct Rule
+    {
+        public Action<GameObject, int, int, int> Method { get; set; }
+        public GameObject ZoneTile { get; set; }
+        public int Axis { get; set; }
+        public int Position { get; set; }
+        public int Azimuth { get; set; }
+
+        internal Rule(
+            Action<GameObject, int, int, int> method,
+            GameObject zoneTile,
+            int axis,
+            int position,
+            int azimuth)
+        {
+            Method = method;
+            ZoneTile = zoneTile;
+            Axis = axis;
+            Position = position;
+            Azimuth = azimuth;
+        }
+    }
+}
--- Assets/Scripts/ZoneCreator.cs
+++ Assets/Scripts/ZoneCreator.cs
@@ -1,32 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class ZoneCreator : MonoBehaviour
+public partial class ZoneCreator : MonoBehaviour
 {
-    private struct Rule
-    {
-        public Action<GameObject, int, int, int> Method { get; set; }
-        public GameObject ZoneTile { get; set; }
-        public int Axis { get; set; }
-        public int Position { get; set; }
-        public int Azimuth { get; set; }
-
-        internal Rule(
-            Action<GameObject, int, int, int> method,
-            GameObject zoneTile,
-            int axis,
-            int position,
-            int azimuth)
-        {
-            Method = method;
-            ZoneTile = zoneTile;
-            Axis = axis;
-            Position = position;
-            Azimuth = azimuth;
-        }
-    }
-
     private readonly GameObject spawner;
     private readonly GameObject despawner;
     private readonly int xEnd;
