9202f3929296fe287cb7e10e2a3078a132c3577a
7138gmhampton <0407435@abertay.ac.uk>
2020-03-04 21:52:12 +0000

    Add method to get all of the wait times for each junction and direction

--- Assets/Scripts/MetricsWatcher.cs
+++ Assets/Scripts/MetricsWatcher.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -6,6 +7,7 @@ using UnityEngine;
 public class MetricsWatcher : MonoBehaviour
 {
     public int queueLength;
+    public AllLightsController lightsController;
 
     private Queue<float> journeyTimes = new Queue<float>();
 
@@ -17,6 +19,11 @@ public class MetricsWatcher : MonoBehaviour
     void Update()
     {
         //Debug.Log(reportJourneyTimeMean());
+        //var times = getWaitTimes();
+        //Debug.Log(times[0, 0].Item1);
+        //Debug.Log(times[0, 0].Item2);
+        //Debug.Log(times[0, 0].Item3);
+        //Debug.Log(times[0, 0].Item4);
     }
 
     public void addJourneyTime(float time)
@@ -32,5 +39,20 @@ public class MetricsWatcher : MonoBehaviour
         if (journeyTimes.Count == 0) return 1000f;
 
         return journeyTimes.Average();
+    }
+
+    private Tuple<float,float,float,float>[,] getWaitTimes()
+    {
+        int width = lightsController.reportHighestX() + 1;
+        int height = lightsController.reportHighestY() + 1;
+
+        var waitTimes = new Tuple<float, float, float, float>[height, width];
+
+        for (int y = 0; y < height; ++y)
+            for (int x = 0; x < width; ++x)
+                waitTimes[y, x] = lightsController.reportWaitTimes(x, y);
+        //float northTime = lightsController.fin
+
+        return waitTimes;
     }
 }
