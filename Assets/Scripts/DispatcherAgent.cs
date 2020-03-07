16b401cf35c59fc6e197f659b1b86488f15ae7f2
7138gmhampton <0407435@abertay.ac.uk>
2020-03-07 15:53:07 +0000

    Create dispatcher agent for RL and add method to undertake action

---
+++ Assets/Scripts/DispatcherAgent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class DispatcherAgent : Agent
+{
+    public AllLightsController lightsController;
+
+    public override void InitializeAgent() => base.InitializeAgent();
+
+    public override void AgentAction(float[] vectorAction)
+    {
+        //base.AgentAction(vectorAction);
+        for (int iii = 0; iii < vectorAction.Length; iii++) {
+            if (vectorAction[iii] > 0.5f)
+                lightsController.junctions[iii].Controller.setGreenAlignment(true);
+            else lightsController.junctions[iii].Controller.setGreenAlignment(false);
+        }
+    }
+
+    public override void CollectObservations() => base.CollectObservations();
+}
