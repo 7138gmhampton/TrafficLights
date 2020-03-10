d3ca7ef44675203cff4fe63678f09d217b183b39
7138gmhampton <0407435@abertay.ac.uk>
2020-03-10 21:35:29 +0000

    Stop reward for finishing car

--- Assets/Scripts/DispatcherAgent.cs
+++ Assets/Scripts/DispatcherAgent.cs
@@ -81,7 +81,7 @@ public class DispatcherAgent : Agent
 
     private void finishCar()
     {
-        AddReward(0.1f);
+        //AddReward(0.1f);
 
         if (++carsPassed > carsPerEpisode) Done();
     }
