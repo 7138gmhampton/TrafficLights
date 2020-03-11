06ca2e46751212bebf52f6c0009a5d001756cdee
7138gmhampton <0407435@abertay.ac.uk>
2020-03-11 17:17:55 +0000

    Create script for exper system dispatcher

---
+++ Assets/Scripts/ExpertSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpertSystem : MonoBehaviour
+{
+    public EnvironmentManager environment;
+
+    private void Awake()
+    {
+        environment.setupEnvironment();
+    }
+
+    private void Start()
+    {
+        environment.prepareCarManager();
+    }
+}
