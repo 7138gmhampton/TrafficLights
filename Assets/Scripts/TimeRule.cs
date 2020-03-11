2f9c786c7e747dbb3b883359f69fbc965d662b83
7138gmhampton <0407435@abertay.ac.uk>
2020-03-11 19:04:10 +0000

    Implement EW time rule

--- Assets/Scripts/TimeRule.cs
+++ Assets/Scripts/TimeRule.cs
@@ -21,3 +21,25 @@ internal class TimeRuleNS : Rule
         else return false;
     }
 }
+
+internal class TimeRuleEW : Rule
+{
+    public override void fire()
+    {
+        //throw new System.NotImplementedException();
+        Object.FindObjectOfType<AllLightsController>().switchEW(xPosition, yPosition);
+    }
+
+    public override bool match(params object[] args)
+    {
+        //throw new System.NotImplementedException();
+        float northTime = (float)args[0];
+        float eastTime = (float)args[1];
+        float southTime = (float)args[2];
+        float westTime = (float)args[3];
+        
+        if ((eastTime + westTime) - (northTime + southTime) > 1f)
+            return true;
+        else return false;
+    }
+}
