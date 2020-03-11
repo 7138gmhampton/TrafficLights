9889b69a85c60c7b3060c84f5934b3f305fa56a4
7138gmhampton <0407435@abertay.ac.uk>
2020-03-11 18:40:20 +0000

    Implement abstract class for rules

--- Assets/Scripts/Rule.cs
+++ Assets/Scripts/Rule.cs
@@ -1,3 +1,9 @@
-﻿internal class Rule
+﻿internal abstract class Rule
 {
+    protected int priority;
+
+    public int Priority { get { return priority; } }
+
+    public abstract bool match();
+    public abstract void fire();
 }
