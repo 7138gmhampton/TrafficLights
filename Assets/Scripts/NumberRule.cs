10cc36049696682a5d143aa624879c763f4f5dce
7138gmhampton <0407435@abertay.ac.uk>
2020-03-11 20:17:27 +0000

    Remove data method delegate

--- Assets/Scripts/NumberRule.cs
+++ Assets/Scripts/NumberRule.cs
@@ -4,17 +4,17 @@ using Object = UnityEngine.Object;
 
 internal class NumberRuleNS : Rule
 {
-    private Func<int, int, Tuple<int, int, int, int>> dataMethod;
+    //private Func<int, int, Tuple<int, int, int, int>> dataMethod;
 
     public NumberRuleNS(int x, int y, int priority)
     {
         xPosition = x;
         yPosition = y;
         this.priority = priority;
-        dataMethod = Object.FindObjectOfType<AllLightsController>().reportQueueNumbers;
+        //dataMethod = Object.FindObjectOfType<AllLightsController>().reportQueueNumbers;
     }
 
-    public Func<int, int, Tuple<int, int, int, int>> DataMethod { get { return dataMethod; } }
+    //public Func<int, int, Tuple<int, int, int, int>> DataMethod { get { return dataMethod; } }
 
     public override void fire() => throw new System.NotImplementedException();
     public override bool match(params object[] args) => throw new System.NotImplementedException();
