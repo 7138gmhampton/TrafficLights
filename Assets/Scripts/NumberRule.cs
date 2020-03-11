2986db88a87ab81afacb8e5db626df846464c442
7138gmhampton <0407435@abertay.ac.uk>
2020-03-11 19:53:09 +0000

    Create skeletons for number based rules

---
+++ Assets/Scripts/NumberRule.cs
@@ -0,0 +1,11 @@
+﻿internal class NumberRuleNS : Rule
+{
+    public override void fire() => throw new System.NotImplementedException();
+    public override bool match(params object[] args) => throw new System.NotImplementedException();
+}
+
+internal class NumberRuleEW : Rule
+{
+    public override void fire() => throw new System.NotImplementedException();
+    public override bool match(params object[] args) => throw new System.NotImplementedException();
+}
