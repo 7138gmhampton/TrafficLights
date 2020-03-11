0b29e790d17bbc5973c80c5277ef943f551279db
7138gmhampton <0407435@abertay.ac.uk>
2020-03-11 19:45:16 +0000

    Add main inference loop

--- Assets/Scripts/ExpertSystem.cs
+++ Assets/Scripts/ExpertSystem.cs
@@ -20,4 +20,26 @@ public class ExpertSystem : MonoBehaviour
     {
         environment.prepareCarManager();
     }
+
+    private void Update()
+    {
+        var triggeredRules = new List<Rule>();
+
+        foreach (var rule in ruleBase) {
+            //var junction = lightsController.findJunction(rule.XPosition, rule.YPosition);
+            var waitTimes = lightsController.reportWaitTimes(rule.XPosition, rule.YPosition);
+            if (rule.match(waitTimes.Item1, waitTimes.Item2, waitTimes.Item3, waitTimes.Item4))
+                triggeredRules.Add(rule);
+        }
+
+        int lowestPriority = 1000;
+        foreach (var rule in triggeredRules)
+            if (rule.Priority < lowestPriority) lowestPriority = rule.Priority;
+
+        foreach (var rule in triggeredRules)
+            if (rule.Priority != lowestPriority) triggeredRules.Remove(rule);
+
+        if (triggeredRules.Count > 0)
+            triggeredRules[Random.Range(0, triggeredRules.Count)].fire();
+    }
 }
