9aa635d995605269d835273ab9812402bc01420f
7138gmhampton <0407435@abertay.ac.uk>
2020-03-11 18:43:32 +0000

    Create skeleton of rule based on time difference

---
+++ Assets/Scripts/TimeRule.cs
@@ -0,0 +1,5 @@
+﻿internal class TimeRule : Rule
+{
+    public override void fire() => throw new System.NotImplementedException();
+    public override bool match() => throw new System.NotImplementedException();
+}
