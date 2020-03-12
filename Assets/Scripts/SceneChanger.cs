98703cc972be6f3141521abdb022723f16ad9f00
7138gmhampton <0407435@abertay.ac.uk>
2020-03-12 18:53:23 +0000

    Rename script to Scene Changer

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartControlScene : MonoBehaviour
+public class SceneChanger : MonoBehaviour
 {
     public void StartControl()
     {
