a83f854cd6e9d53b433f4c2401a33431b0537a12
7138gmhampton <0407435@abertay.ac.uk>
2020-03-12 19:41:49 +0000

    End scene when max cars has been reached

--- Assets/Scripts/Experiment.cs
+++ Assets/Scripts/Experiment.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Experiment : MonoBehaviour
 {
-    
+    public int maxCarsInExperiment;
+
+    private int carsPassed = 0;
+
+    private void finishCar()
+    {
+        if (++carsPassed >= maxCarsInExperiment) SceneManager.LoadScene("MenuScene");
+    }
 }
