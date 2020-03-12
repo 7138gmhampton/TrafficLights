8590e7722ae8a84ff4bb8ce444fe731dcb025478
7138gmhampton <0407435@abertay.ac.uk>
2020-03-12 19:10:30 +0000

    Change to Scene Changer as name and add transitions for inference and expert systems

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void startControl()
+    {
+        SceneManager.LoadScene("ControlScene");
+    }
+
+    public void startExpert()
+    {
+        SceneManager.LoadScene("ExpertScene");
+    }
+
+    public void startReinforcement()
+    {
+        SceneManager.LoadScene("InferenceScene");
+    }
+}
